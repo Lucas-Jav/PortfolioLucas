aa4eb2b80a3f9af8db3bccaeb726ead165a4acf8
lucas oliveira <lucas.meialua@outlook.com>
2023-03-10 22:09:31 -0300

    :truck: Adicionando rotas

---
+++ src/router.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+
+const Rotas = () => {
+    <BrowserRouter>
+        <Routes>
+            
+        </Routes>
+    </BrowserRouter>
+}
