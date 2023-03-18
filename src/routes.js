28b356b10163d1b26842e7ec23652a49444ffb57
lucas oliveira <lucas.meialua@outlook.com>
2023-03-18 13:44:03 -0300

    :bricks: ci: pagina 404

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Route, BrowserRouter, Routes } from "react-router-dom";
+import { Route, BrowserRouter, Routes, Navigate } from "react-router-dom";
+import NotFound from "./screens/404";
 import HomeScreen from "./screens/home";
 import PerfisScreen from "./screens/perfis";
 import { AboutScreen, ContractScreen, SkillScreen, WorksScreen } from "./screens/sections";
@@ -12,6 +13,8 @@ const Routers = () => (
             <Route exact path="/skills" element={<SkillScreen/>}/>
             <Route exact path="/works" element={<WorksScreen/>}/>
             <Route exact path="/contract" element={<ContractScreen/>}/>
+            <Route exact path="/404" element={<NotFound/>}/>
+            <Route exact path="*" element={<Navigate to="/404"/>}/>
         </Routes>
     </BrowserRouter>
 )
